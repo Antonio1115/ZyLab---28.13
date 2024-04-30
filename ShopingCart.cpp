0cbae02028b81531cbae6cdbd0cbed139e11c284
Pierre Martinez <pxmartinez@smu.edu>
2024-04-30 17:51:12 -0500

    h

---
+++ ShopingCart.cpp
@@ -0,0 +1,23 @@
+/* Type your code here */
+#include <iostream>
+using namespace std;
+
+#include "ShoppingCart.h"
+
+ShoppingCart::ShoppingCart(string customerName, string customerDate){
+    this->customerName = customerName;
+    this->currentDate = customerDate;
+}
+
+string ShoppingCart::GetCustomerName(){
+    return customerName;
+}
+string ShoppingCart::GetDate(){
+    return currentDate;
+}
+void ShoppingCart::AddItem(ItemToPurchase item){
+    cartItems.push_back(item);
+}
+void ShoppingCart::RemoveItem(ItemToPurchase item){
+    
+}
