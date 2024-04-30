413b011424ccc4a09bcd59ffddec9225d47bc5d4
Antonio Flores <1115tonyflores@gmail.com>
2024-04-30 13:48:36 -0500

    ItemToPurchase classes and most of the ShoppingCart classes

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,46 @@
 /* Type your code here */
+
+#include <iostream>
+using namespace std;
+
+#include "ItemToPurchase.h"
+
+    ItemToPurchase::ItemToPurchase(string userName, string userDescription, int userPrice, int userQuantity){
+    this->name = userName;
+    this->description = userDescription;
+    this->price = userPrice;
+    this->quantity = userQuantity;
+    }
+
+    ItemToPurchase::ItemToPurchase(string userName, int userPrice, int userQuantity){
+   
+   this->name = userName;
+   this->price = userPrice;
+   this->quantity = userQuantity;
+   
+    }    
+   
+    void ItemToPurchase::SetPrice(int userPrice){
+    this->price = userPrice;  
+    }
+    void ItemToPurchase::SetName(string userName){
+      this->name = userName;
+    }
+    void ItemToPurchase::SetQuantity(int userQuantity){
+      this->quantity = userQuantity;
+    }
+    void ItemToPurchase::SetDescription(string userDescription){
+    this->description = userDescription;
+    }
+    int ItemToPurchase::GetPrice(){
+      return price;
+    }
+    string ItemToPurchase::GetName(){
+      return name;
+    }
+    int ItemToPurchase::GetQuantity(){
+      return quantity;
+    }    
+    string ItemToPurchase::GetDescription(){
+        return description;
+    }
--- ShopingCart.h
+++ ShopingCart.h
@@ -1,1 +1,41 @@
 /* Type your code here */
+
+#ifndef SHOPPING_CART_H
+#define SHOPPING_CART_H
+
+#include <string>
+#include <vector>
+#include "ItemToPurchase.h"
+using namespace std;
+
+class ShoppingCart{
+
+public:
+
+ShoppingCart(string customerName = "none", string customerDate = "January 1, 2016");
+
+string GetCustomerName();
+string GetDate();
+
+void AddItem(ItemToPurchase item);
+void RemoveItem(ItemToPurchase item);
+void ModifyItem(ItemToPurchase item);
+
+void GetNumItemsInCart();
+void GetCostOfCart();
+
+void PrintTotal();
+void PrintDescriptions();
+
+
+
+
+private:
+
+    string customerName;
+    string currentDate;
+    vector<ItemToPurchase> cartItems;
+
+};
+
+#endif
