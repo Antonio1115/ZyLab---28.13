d0dbb2bce38a18b147c3ee6562c61184a1fdf71c
Antonio Flores <1115tonyflores@gmail.com>
2024-04-30 22:18:34 -0500

    All around fix

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -1,43 +1,40 @@
-/* Type your code here */
-
 #ifndef ITEM_TO_PURCHASE_H
 #define ITEM_TO_PURCHASE_H
 
 #include <string>
 using namespace std;
  
-class ItemToPurchase {
+ class ItemToPurchase {
 
-    public:
+public:
 
-        ItemToPurchase(string userName, string userDescription = "none", int userPrice = 0, int userQuantity = 0);
-        ItemToPurchase(string userName = "none", int userPrice = 0, int userQuantity = 0);
+    ItemToPurchase(string userName, string userDescription = "none", int userPrice = 0, int userQuantity = 0);
+    ItemToPurchase(string userName = "none", int userPrice = 0, int userQuantity = 0);
+   
+   void SetPrice(int userPrice);
+   void SetName(string userName);
+   void SetQuantity(int userQuantity); 
+   void SetCost(int userPrice, int userQuantity);
+   void SetDescription(string userDescription);
+   
+    int GetPrice();
+    string GetName() const;
+    int GetQuantity();
+    int GetCost();
+    string GetDescription() const;
+
+    void PrintItemCost();
+    void PrintItemDescription();
+
+
     
-        void SetPrice(int userPrice);
-        void SetName(string userName);
-        void SetQuantity(int userQuantity); 
-        void SetCost(int userPrice, int userQuantity);
-        void SetDescription(string userDescription);
-    
-        int GetPrice();
-        string GetName();
-        int GetQuantity();
-        string GetDescription();
+private:
 
-        int GetCost();
-
-        void PrintItemCost();
-        void PrintItemDescription();
-
-
-
-    private:
-
-        string name;
-        int price;
-        int quantity;
-        int cost;
-        string description;
+    string name;
+    int price;
+    int quantity;
+    int cost;
+    string description;
 
  };
 
